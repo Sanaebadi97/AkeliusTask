31e32d5a409c55492e2f500bbb16099577353177
sanaebadi <sana@journeyhero.net>
2022-11-01 18:12:03 +0200

    fixed gradle and convert to Kotlin.DSL

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,6 +20,8 @@ pluginManagement {
             // Use `resolutionStrategy` to define the Hilt plugin and its coordinate
             if (requested.id.id == "dagger.hilt.android.plugin")
                 useModule("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
+            if (requested.id.id == "androidx.navigation.safeargs")
+                useModule("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
             if (requested.id.id == "com.google.gms.google-services")
                 useModule("com.google.gms:google-services:4.3.10")
 
